cf9c59a621906a3d233c4d53f9b5420659ba4cae
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-03-18 17:12:16 +0100

    Played around with http://alvinalexander.com/scala/future-example-scala-cookbook-oncomplete-callback.

--- code/Main.scala
+++ code/Main.scala
@@ -1,10 +1,33 @@
 package de.hska.wifl1011.seminararbeit
 
+import scala.concurrent.future
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+import scala.util.Random
+
 object Main {
 
   def main(args: Array[String]) {
 
-    println("Hallo, welt")
+    val f: Future[Int] = future {
+      Thread.sleep(Random.nextInt(500))
+      42
+    }
 
+    f.onComplete {
+      case Success(value) => println("Got callback:" + value)
+      case Failure(e) => e.printStackTrace
+    }
+
+    // do the rest of your work
+    println("A ..."); Thread.sleep(100)
+    println("B ..."); Thread.sleep(100)
+    println("C ..."); Thread.sleep(100)
+    println("D ..."); Thread.sleep(100)
+    println("E ..."); Thread.sleep(100)
+    println("F ..."); Thread.sleep(100)
+
+    Thread.sleep(2000)
   }
 }
