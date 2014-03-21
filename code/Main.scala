1e5d7c5b59c861fba61975fa318cc7743cd6f4ce
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-03-21 23:14:25 +0100

    Playing around..

--- code/Main.scala
+++ code/Main.scala
@@ -8,23 +8,42 @@ import scala.util.Random
 
 object Main {
 
+  def doSomeWork() {
+    println("A ..."); Thread.sleep(100)
+    println("B ..."); Thread.sleep(100)
+    println("C ..."); Thread.sleep(100)
+    println("D ..."); Thread.sleep(100)
+    println("E ..."); Thread.sleep(100)
+    println("F ..."); Thread.sleep(100)
+  }
+  
+  def isProfitable(quote): Boolean {
+    if ((quote) > 40) {
+      true
+    } else {
+      false
+    }
+  }
+
   def main(args: Array[String]) {
 
+    println("Trying to get the exchange-rate")
     val exchangeRate: Future[Int] = future {
       Thread.sleep(Random.nextInt(500))
 
       // sometimes things work, sometimes they don't...
-//      if (Random.nextInt(6) < 4) {
+      if (Random.nextInt(6) < 4) {
         Random.nextInt(100) 
-//      } else {
-//        throw new Exception
-//      }
+      } else {
+        throw new Exception
+      }
     }
 
     exchangeRate onComplete {
       case Success(value) => {
+        println("Trying to book a flight")
         val bookFlight: Future[Int] = future {
-          Thread.sleep(1000)
+          Thread.sleep(Random.nextInt(500))
           1
         }
 
@@ -37,12 +56,7 @@ object Main {
     }
 
     // do the rest of your work
-    println("A ..."); Thread.sleep(100)
-    println("B ..."); Thread.sleep(100)
-    println("C ..."); Thread.sleep(100)
-    println("D ..."); Thread.sleep(100)
-    println("E ..."); Thread.sleep(100)
-    println("F ..."); Thread.sleep(100)
+    doSomeWork()
 
     // keep the jvm running
     Thread.sleep(3000)
