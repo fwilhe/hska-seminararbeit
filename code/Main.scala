268243b4c7627815a64879bc4d9de4cbfb3eb522
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-03-31 18:12:07 +0200

    Began playing around with promises, made a folder for the holiday-usecase of futures.

--- code/Main.scala
+++ code/Main.scala
@@ -1,64 +1,57 @@
 package de.hska.wifl1011.seminararbeit
 
-import scala.concurrent.future
-import scala.concurrent.Future
+import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 import scala.util.Random
 
 object Main {
 
-  def futureDemo() {
-    println("Trying to get the exchange-rate")
-    val exchangeRate: Future[Int] = future {
-      Thread.sleep(Random.nextInt(500))
-
-      // sometimes things work, sometimes they don't...
-      if (Random.nextInt(6) < 4) {
-        Random.nextInt(100) 
-      } else {
-        throw new Exception
-      }
-    }
-
-    exchangeRate onComplete {
-      case Success(value) => {
-        println("Trying to book a flight")
-        val bookFlight: Future[Int] = future {
-          Thread.sleep(Random.nextInt(500))
-          1
-        }
-
-        bookFlight onSuccess {
-          case msg => println("We can fly to hawaii now! \\o/")
-        }
-      }
-
-      case Failure(e) => println("Ooops, an error occured.")
-    }
-
-    // do the rest of your work
-    doSomeWork()
-
-    // keep the jvm running
-    Thread.sleep(3000)  
+  def produceSomething() = {
+    println("** produceSomething **")
+    Thread.sleep(200)
+    42
   }
-
-  def doSomeWork() {
+  
+  def continueDoingSomethingUnrelated() {
+    println("** continueDoingSomethingUnrelated **")
     println("A ..."); Thread.sleep(100)
     println("B ..."); Thread.sleep(100)
     println("C ..."); Thread.sleep(100)
     println("D ..."); Thread.sleep(100)
-    println("E ..."); Thread.sleep(100)
-    println("F ..."); Thread.sleep(100)
   }
   
-  def promiseDemo() {
-  
+  def startDoingSomething() {
+    println("** startDoingSomething **")
+    println("α ..."); Thread.sleep(100)
+    println("β ..."); Thread.sleep(100)
+    println("γ ..."); Thread.sleep(100)
+    println("δ ..."); Thread.sleep(100)
   }
+  
+  def doSomethingWithResult() {
+    println("** doSomethingWithResult **")
+  }
+  
 
   def main(args: Array[String]) {
-    //futureDemo()
-    promiseDemo()
+    val p = promise[Integer]
+    val f = p.future
+    
+    val producer = future {
+      val r = produceSomething()
+	p success r
+	continueDoingSomethingUnrelated()
+      }
+      
+    val consumer = future {
+      startDoingSomething()
+      f onSuccess {
+        case r => doSomethingWithResult()
+      }
+    }
+    
+    // keep the jvm running
+    Thread.sleep(3000) 
   }
 }
---
+++ code/holiday/Main.scala
@@ -0,0 +1,55 @@
+package de.hska.wifl1011.seminararbeit
+
+import scala.concurrent.future
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+import scala.util.Random
+
+object Main {
+
+  def doSomeWork() {
+    println("A ..."); Thread.sleep(100)
+    println("B ..."); Thread.sleep(100)
+    println("C ..."); Thread.sleep(100)
+    println("D ..."); Thread.sleep(100)
+    println("E ..."); Thread.sleep(100)
+    println("F ..."); Thread.sleep(100)
+  }
+
+  def main(args: Array[String]) {
+    println("Trying to get the exchange-rate")
+    val exchangeRate: Future[Int] = future {
+      Thread.sleep(Random.nextInt(500))
+
+      // sometimes things work, sometimes they don't...
+      if (Random.nextInt(6) < 4) {
+        Random.nextInt(100) 
+      } else {
+        throw new Exception
+      }
+    }
+
+    exchangeRate onComplete {
+      case Success(value) => {
+        println("Trying to book a flight")
+        val bookFlight: Future[Int] = future {
+          Thread.sleep(Random.nextInt(500))
+          1
+        }
+
+        bookFlight onSuccess {
+          case msg => println("We can fly to hawaii now! \\o/")
+        }
+      }
+
+      case Failure(e) => println("Ooops, an error occured.")
+    }
+
+    // do the rest of your work
+    doSomeWork()
+
+    // keep the jvm running
+    Thread.sleep(3000)  
+  }
+}
