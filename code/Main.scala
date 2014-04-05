019673a99c7b955620a56077da062450e6530350
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-04-05 11:11:46 +0200

    Cleanup the template.

--- code/Main.scala
+++ code/Main.scala
@@ -1,17 +1,24 @@
 package de.hska.wifl1011.seminararbeit
 
-import scala.concurrent.{ future, promise }
+import scala.concurrent.{ future, Future }
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Failure, Success }
+import scala.util.Random
 
 object Main {
 
   def main(args: Array[String]) {
-    val myPromise = promise[Integer]
-    val myFuture = myPromise.future
 
+    val f: Future[Integer] = future {
+      Thread.sleep(Random.nextInt(500))
+      42
+    }
     
+    f onSuccess {
+      case value => println(value)
+    }
+
     // keep the jvm running
-    Thread.sleep(3000)
+    Thread.sleep(1000)
   }
 }
