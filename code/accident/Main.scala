8f007b2963636086e52d7995ddb547e72b4b523e
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-04-05 11:20:49 +0200

    Prettifying

--- code/accident/Main.scala
+++ code/accident/Main.scala
@@ -2,33 +2,34 @@ package de.hska.wifl1011.seminararbeit
 
 import scala.concurrent.{ future, Future }
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{ Failure, Success }
+import scala.util.Success
 import scala.util.Random
 
 object Main {
 
   def main(args: Array[String]) {
 
-    val doctor1: Future[Integer] = future {
+    val doctor1: Future[String] = future {
       Thread.sleep(Random.nextInt(500))
-      42
+      "doctor from the nearby hospital"
     }
-    val doctor2: Future[Integer] = future {
+    
+    val doctor2: Future[String] = future {
       Thread.sleep(Random.nextInt(500))
-      23
+      "doctor from the quick response team"
     }
-    val doctor3: Future[Integer] = future {
+    
+    val doctor3: Future[String] = future {
       Thread.sleep(Random.nextInt(500))
-      1337
+      "doctor who just happens to enjoy the same trip"
     }
     
     val doctors = List(doctor1, doctor2, doctor3)
 
-    // Get any information which is available via several chanells, don't care who brings it
     val f = Future.firstCompletedOf(doctors)
 
     f onSuccess {
-      case value => println(value)
+      case value => println("The first doctor is here.\nIt's the " + value + ".")
     }
 
     // keep the jvm running
