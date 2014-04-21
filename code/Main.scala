9ed7c4afdfe66d07ac9115209a58eafdecd7a023
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-04-21 15:32:44 +0200

    Reset Template-file.

--- code/Main.scala
+++ code/Main.scala
@@ -6,37 +6,10 @@ import scala.util.Random
 
 object Main {
 
-  type Tire = String
-
-  def getNewTireAsFuture(): Future[Tire] = future {
-    Thread.sleep(Random.nextInt(100))
-    "Tire By Brand XY"
-  }
-  
-  def replaceTire(tire: Tire): Future[Boolean] = future {
-    Thread.sleep(Random.nextInt(100))
-    true  
-  }
-  
-  def changeTires() {
-    for {
-      tire <- getNewTireAsFuture()
-      success <- replaceTire(tire)
-    } yield println("got response " + success)
-  }
-  
-  def doSomeWork() {
-    println("A ..."); Thread.sleep(100)
-    println("B ..."); Thread.sleep(100)
-    println("C ..."); Thread.sleep(100)
-    println("D ..."); Thread.sleep(100)
-    println("E ..."); Thread.sleep(100)
-    println("F ..."); Thread.sleep(100)
-  }
 
   def main(args: Array[String]) {
-    changeTires()
-    doSomeWork()
+
+
     
     Thread.sleep(500)
   }
