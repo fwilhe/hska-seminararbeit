0d1cf3cb18cbeb30b8c70de7e1dfa6a5f42f452d
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2014-04-27 08:44:11 +0200

    Rewrote the chapter on combinators.

--- code/harvestluna/Main.scala
+++ code/harvestluna/Main.scala
@@ -3,6 +3,7 @@ package de.hska.wifl1011.seminararbeit
 import scala.concurrent.{ future, Future }
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
+import scala.util.{ Failure, Success }
 
 object Main {
 
@@ -12,14 +13,28 @@ object Main {
     Thread.sleep(Random.nextInt(100))
     new Cucumber(Random.nextInt(40))
   }
+  
+  def isStraightEnough(cucumber: Cucumber): Boolean = {
+    cucumber.bendingInDegree < 20
+  }
+  
+  def loadOnBox(cucumber: Cucumber) {
+  
+  }
 
   def main(args: Array[String]) {
 
-    val cucumbers = List(harvestCucumber(), harvestCucumber(), harvestCucumber())
+    val cucumberFuture = harvestCucumber()
     
-    println(cucumbers)
-    // cucumbers.filter(() => )
-    // cucumbers.filter((c: Cucumber) => c.bendingInDegree > 20)
+    val harvestFuture = cucumberFuture.map {
+      cucumber => if (isStraightEnough(cucumber)) loadOnBox(cucumber)
+      else throw new Exception("Can't sell this one")
+    }
+    
+    harvestFuture.onComplete {
+      case Success(_) => println("Harvested a Cucumber")
+      case Failure(ex) => println(ex)
+    }
     
     Thread.sleep(500)
   }
